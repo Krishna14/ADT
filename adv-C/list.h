c1a8ed22e8b00a90c64b8ee7575ac17a62b4e53e
Krishna14 <krishna1994sree@gmail.com>
2014-01-27 20:44:32 +0530

    First Commit

---
+++ adv-C/list.h
@@ -0,0 +1,17 @@
+#ifndef _LIST_H
+#define _LIST_H//Define _LIST_H if not defined !
+
+typedef struct node node;
+typedef node *nodeptr;
+
+struct node{
+	int data;
+	nodeptr next;
+};
+
+void list_push(nodeptr *,int );
+void list_pop(nodeptr *);
+void list_delete(nodeptr *);
+int list_length(nodeptr *);
+
+#endif
